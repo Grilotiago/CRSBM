fa1c714eb381d47a5d84de5b26827cd019f3181c
Grilotiago <tg.grilo@gmail.com>
2026-09-17 09:44:28 -0300

    ajuste pesquisa

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const mysql = require('mysql2/promise');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Accept']
+}));
 app.use(express.json());
 
 const pool = mysql.createPool({
